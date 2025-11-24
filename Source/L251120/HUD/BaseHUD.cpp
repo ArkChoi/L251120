7709e53e7c0852caec93e6635fcbd24b1210453e
ArkChoi <ark.goya@gmail.com>
2025-11-24 17:44:49 +0900

    AimSetting

--- Source/L251120/HUD/BaseHUD.cpp
+++ Source/L251120/HUD/BaseHUD.cpp
@@ -3,6 +3,8 @@
 
 #include "BaseHUD.h"
 #include "Engine/Canvas.h"
+#include "../LMyCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 void ABaseHUD::DrawHUD()
 {
@@ -14,15 +16,45 @@ void ABaseHUD::DrawHUD()
 	int32 CenterY = Canvas->SizeY / 2;
 	int32 DrawSize = 2;
 
+	ALMyCharacter* TempCharacter = Cast<ALMyCharacter>(GetOwningPlayerController()->GetPawn());
+	if (TempCharacter)
+	{
+		float GroundSpeed = TempCharacter->GetCharacterMovement()->Velocity.Size2D();
+		if (GroundSpeed > 0 && TempCharacter->bIsRun)
+		{
+			DrawSize = 4;
+		}
+		else if (GroundSpeed > 0)
+		{
+			DrawSize = 3;
+		}
+		else
+		{
+			DrawSize = 2;
+		}
+	}
+
 	Draw2DLine(CenterX - (UnitX * DrawSize),
 		CenterY,
-		CenterX + (UnitX * DrawSize),
+		CenterX - (UnitX * (DrawSize - 2)),
+		CenterY,
+		FColor::Red);
+
+	Draw2DLine(CenterX + (UnitX * DrawSize),
+		CenterY,
+		CenterX + (UnitX * (DrawSize - 2)),
 		CenterY,
 		FColor::Red);
 
 	Draw2DLine(CenterX,
 		CenterY - (UnitX * DrawSize),
 		CenterX,
+		CenterY - (UnitX * (DrawSize - 2)),
+		FColor::Red);
+
+	Draw2DLine(CenterX,
 		CenterY + (UnitX * DrawSize),
+		CenterX,
+		CenterY + (UnitX * (DrawSize - 2)),
 		FColor::Red);
 }
